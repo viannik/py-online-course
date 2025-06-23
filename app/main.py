d11744ec972459935028d3bbe4db47f4cc7f90f2
viannik <ivan.nikitski@gmail.com>
2025-06-23 23:46:42 +0300

    Implement OnlineCourse class with initialization and utility methods

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
+import math
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str,
+                 description: str,
+                 weeks: str | int | float) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = int(weeks)
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return math.ceil(days / 7)
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> "OnlineCourse":
+        return cls(course_dict["name"],
+                   course_dict["description"],
+                   cls.days_to_weeks(course_dict["days"]))
